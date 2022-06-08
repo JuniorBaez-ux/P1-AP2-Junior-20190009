631880b12c879e8b18af1ade7ec06651a03951ef
Junior Baez <junior_baez@ucne.edu.do>
2022-06-08 18:34:17 -0400

    Renombrando los campos necesarios

--- app/src/main/java/com/example/p1_ap2_junior_20190009/data/repository/PrestamosRepository.kt
+++ app/src/main/java/com/example/p1_ap2_junior_20190009/data/repository/PrestamosRepository.kt
@@ -5,7 +5,7 @@ import com.example.p1_ap2_junior_20190009.model.Prestamos
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
-class Parcial1Ap2Repository @Inject constructor(
+class PrestamosRepository @Inject constructor(
     val prestamosDao: PrestamosDao
 ) {
     suspend fun Insertar(prestamos: Prestamos)= prestamosDao.Insertar(prestamos)
