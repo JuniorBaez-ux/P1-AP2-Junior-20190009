1ed63b4182c3f369ebe1862170f7a511586d4c51
Junior Baez <junior_baez@ucne.edu.do>
2022-06-08 18:29:07 -0400

    Renombrando los campos necesarios

---
+++ app/src/main/java/com/example/p1_ap2_junior_20190009/data/PrestamosDb.kt
@@ -0,0 +1,14 @@
+package com.example.p1_ap2_junior_20190009.data
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.example.p1_ap2_junior_20190009.model.Prestamos
+
+@Database(
+    entities = [Prestamos::class],
+    version = 1
+)
+abstract class PrestamosDb: RoomDatabase() {
+    abstract  val prestamosDao: PrestamosDao
+
+}
