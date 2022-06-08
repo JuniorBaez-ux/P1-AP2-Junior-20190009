b69128d23a4c55b2062db62129b6882e476577be
Junior Baez <junior_baez@ucne.edu.do>
2022-06-08 18:29:20 -0400

    Renombrando los campos necesarios

--- app/src/main/java/com/example/p1_ap2_junior_20190009/data/repository/Parcial1Ap2Repository.kt
+++ app/src/main/java/com/example/p1_ap2_junior_20190009/data/repository/Parcial1Ap2Repository.kt
@@ -1,18 +1,18 @@
 package com.example.p1_ap2_junior_20190009.data.repository
 
-import com.example.p1_ap2_junior_20190009.data.Parcial1Ap2Dao
-import com.example.p1_ap2_junior_20190009.model.Parcial1Ap2
+import com.example.p1_ap2_junior_20190009.data.PrestamosDao
+import com.example.p1_ap2_junior_20190009.model.Prestamos
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class Parcial1Ap2Repository @Inject constructor(
-    val parcial1Ap2Dao: Parcial1Ap2Dao
+    val prestamosDao: PrestamosDao
 ) {
-    suspend fun Insertar(parcial1ap2: Parcial1Ap2)= parcial1Ap2Dao.Insertar(parcial1ap2)
+    suspend fun Insertar(prestamos: Prestamos)= prestamosDao.Insertar(prestamos)
 
-    fun Buscar(parcial1ap2: Int): Flow<Parcial1Ap2>   =  parcial1Ap2Dao.Buscar(parcial1ap2)
+    fun Buscar(prestamos: Int): Flow<Prestamos>   =  prestamosDao.Buscar(prestamos)
 
-    suspend fun Eliminar(parcial1ap2: Parcial1Ap2)= parcial1Ap2Dao.Eliminar(parcial1ap2)
+    suspend fun Eliminar(prestamos: Prestamos)= prestamosDao.Eliminar(prestamos)
 
-    fun GetLista(): Flow<List<Parcial1Ap2>> = parcial1Ap2Dao.GetLista()
+    fun GetLista(): Flow<List<Prestamos>> = prestamosDao.GetLista()
 }
